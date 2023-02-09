65a4b176086e67a74708281d0289780e0b6dede1
Frank Tse <franktseme@gmail.com>
2023-02-09 23:27:57 +0800

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="space-y-1 py-4 text-center text-xs leading-5 text-slate-500">
+      <div className="container mx-auto flex flex-col gap-1 px-4">
+        <p>©&nbsp;Cygames,&nbsp;Inc.</p>
+        <p>
+          当サイト上で使用しているゲーム画像の著作権および商標権、その他知的財産権は、当該コンテンツの提供元に帰属します。
+        </p>
+      </div>
+    </footer>
+  )
+}
