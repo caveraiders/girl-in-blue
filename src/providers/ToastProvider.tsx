00fecd4ad05b06f9d0366c185507da0e0384c69a
Frank Tse <franktseme@gmail.com>
2023-02-24 22:14:17 +0800

    feat: improve Toast functionality to be globally accessible

---
+++ src/providers/ToastProvider.tsx
@@ -0,0 +1,42 @@
+'use client'
+import ToastContext from '@/contexts/ToastContext'
+import { useState } from 'react'
+import Toast from '@/components/Toast'
+
+type ToastProviderProps = {
+  children: React.ReactNode
+}
+
+type Toast = {
+  open: boolean
+  title: string
+  description: string
+  direction: 'top' | 'center' | 'bottom' | 'right'
+  type: 'success' | 'error'
+}
+
+const ToastProvider = ({ children }: ToastProviderProps) => {
+  const [toast, setToast] = useState<Toast>({
+    open: false,
+    title: 'Title',
+    description: 'Description',
+    direction: 'top',
+    type: 'success',
+  })
+
+  return (
+    <ToastContext.Provider value={{ setToast }}>
+      <Toast
+        title={toast.title}
+        description={toast.description}
+        direction={toast.direction}
+        open={toast.open}
+        onOpenChange={(open: boolean) => setToast({ ...toast, open })}
+      >
+        {children}
+      </Toast>
+    </ToastContext.Provider>
+  )
+}
+
+export default ToastProvider
