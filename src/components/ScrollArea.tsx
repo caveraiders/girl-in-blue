7e4b10ecf406c223f5aca610f8d1c2db9eb5f579
Frank Tse <franktseme@gmail.com>
2023-02-24 23:48:19 +0800

    feat: add ScrollArea component

---
+++ src/components/ScrollArea.tsx
@@ -0,0 +1,28 @@
+import * as RadixUiScrollArea from '@radix-ui/react-scroll-area'
+
+type ScrollAreaProps = {
+  children: React.ReactNode
+}
+
+const ScrollArea = ({ children }: ScrollAreaProps) => (
+  <RadixUiScrollArea.Root className="h-full w-full overflow-hidden bg-transparent">
+    <RadixUiScrollArea.Viewport className="h-full w-full">
+      <div className="py-[15px] px-5">{children}</div>
+    </RadixUiScrollArea.Viewport>
+    <RadixUiScrollArea.Scrollbar
+      className="flex touch-none select-none bg-slate-100 p-0.5 transition-colors duration-[160ms] ease-out hover:bg-slate-200 data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
+      orientation="vertical"
+    >
+      <RadixUiScrollArea.Thumb className="relative flex-1 rounded-lg bg-slate-400 before:absolute before:top-1/2 before:left-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
+    </RadixUiScrollArea.Scrollbar>
+    <RadixUiScrollArea.Scrollbar
+      className="flex touch-none select-none bg-slate-100 p-0.5 transition-colors duration-[160ms] ease-out hover:bg-slate-200 data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
+      orientation="horizontal"
+    >
+      <RadixUiScrollArea.Thumb className="relative flex-1 rounded-lg bg-slate-400 before:absolute before:top-1/2 before:left-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
+    </RadixUiScrollArea.Scrollbar>
+    <RadixUiScrollArea.Corner className="bg-black" />
+  </RadixUiScrollArea.Root>
+)
+
+export default ScrollArea
