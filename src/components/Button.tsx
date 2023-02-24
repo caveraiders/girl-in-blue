c4c889ec603223d499f4540d5d147930a4180232
kimberly <kimberlylam33@gmail.com>
2023-02-24 20:43:42 +0800

    refactor: improve Button and Toast components to reduce code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import classnames from 'classnames'
+import { forwardRef } from 'react'
 
 type Props = {
   children: React.ReactNode
@@ -20,65 +21,72 @@ type AppearanceClassNames = {
   }
 }
 
-const Button = ({
-  children,
-  appearance = 'primary',
-  isFullWidth = false,
-  variant = 'filled',
-  isDisabled = false,
-  onClick = () => {},
-  isOnlyIcon = false,
-  type = 'button',
-  size = 'lg',
-}: Props) => {
-  const appearanceClassNames: AppearanceClassNames = {
-    primary: {
-      filled:
-        'bg-sky-500 text-white dark:bg-sky-400 hover:bg-sky-600 dark:hover:bg-sky-500 focus:outline-none focus:ring-4 dark:focus:ring-sky-800 focus:ring-sky-300 disabled:bg-sky-300 dark:disabled:bg-sky-300 disabled:cursor-not-allowed',
-      outlined:
-        'bg-transparent text-sky-500 border border-sky-500 dark:border-sky-300 dark:text-sky-300 hover:bg-sky-50 dark:hover:bg-sky-300/10 focus:outline-none focus:ring-4 dark:focus:ring-sky-800 focus:ring-sky-300 disabled:bg-sky-50 dark:disabled:bg-sky-300/10 disabled:cursor-not-allowed',
-      text: 'bg-transparent text-sky-500 dark:text-sky-300 hover:bg-sky-50 dark:hover:bg-sky-300/10 disabled:bg-transparent disabled:text-sky-300 disabled:dark:text-sky-600 dark:disabled:bg-transparent disabled:cursor-not-allowed',
-    },
-    secondary: {
-      filled:
-        'bg-slate-700 text-white dark:bg-slate-800 hover:bg-slate-600 dark:hover:bg-slate-700 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-slate-300 disabled:bg-slate-600 dark:disabled:bg-slate-600 disabled:cursor-not-allowed',
-      outlined:
-        'bg-transparent text-slate-700 border border-slate-700 dark:border-slate-600 dark:text-slate-400 hover:bg-slate-100 dark:hover:bg-slate-300/10 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-slate-300 disabled:bg-slate-100 dark:disabled:bg-slate-300/10 disabled:cursor-not-allowed',
-      text: 'bg-transparent text-slate-700 dark:text-slate-400 hover:bg-slate-50 dark:hover:bg-slate-300/10 disabled:bg-transparent disabled:text-slate-400 disabled:dark:text-slate-600 dark:disabled:bg-transparent disabled:cursor-not-allowed',
-    },
-    danger: {
-      filled:
-        'bg-red-500 text-white dark:bg-red-400 hover:bg-red-600 dark:hover:bg-red-500 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-red-300 disabled:bg-red-300 dark:disabled:bg-red-300 disabled:cursor-not-allowed',
-      outlined:
-        'bg-transparent text-red-500 border border-red-500 dark:border-red-300 dark:text-red-300 hover:bg-red-50 dark:hover:bg-red-300/10 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-red-300 disabled:bg-red-50 dark:disabled:bg-red-300/10 disabled:cursor-not-allowed',
-      text: 'bg-transparent text-red-500 dark:text-red-300 hover:bg-red-50 dark:hover:bg-red-300/10 disabled:bg-transparent disabled:text-red-300 disabled:dark:text-red-900 dark:disabled:bg-transparent disabled:cursor-not-allowed',
-    },
-  }
-
-  const buttonClassNames = classnames(
-    'flex items-center justify-center gap-1.5 rounded-lg font-medium',
-    appearanceClassNames[appearance][variant],
+const Button = forwardRef<HTMLButtonElement, Props>(
+  (
     {
-      'w-full': isFullWidth,
-      'h-6 px-4 text-xs': size === 'sm',
-      'h-8 px-5 text-sm': size === 'md',
-      'h-10 px-6 text-sm': size === 'lg',
+      children,
+      appearance = 'primary',
+      isFullWidth = false,
+      variant = 'filled',
+      isDisabled = false,
+      onClick = () => {},
+      isOnlyIcon = false,
+      type = 'button',
+      size = 'lg',
     },
-    {
-      'h-10 w-10 px-0': isOnlyIcon,
-    },
-  )
+    ref,
+  ) => {
+    const appearanceClassNames: AppearanceClassNames = {
+      primary: {
+        filled:
+          'bg-sky-500 text-white dark:bg-sky-400 hover:bg-sky-600 dark:hover:bg-sky-500 focus:outline-none focus:ring-4 dark:focus:ring-sky-800 focus:ring-sky-300 disabled:bg-sky-300 dark:disabled:bg-sky-300 disabled:cursor-not-allowed',
+        outlined:
+          'bg-transparent text-sky-500 border border-sky-500 dark:border-sky-300 dark:text-sky-300 hover:bg-sky-50 dark:hover:bg-sky-300/10 focus:outline-none focus:ring-4 dark:focus:ring-sky-800 focus:ring-sky-300 disabled:bg-sky-50 dark:disabled:bg-sky-300/10 disabled:cursor-not-allowed',
+        text: 'bg-transparent text-sky-500 dark:text-sky-300 hover:bg-sky-50 dark:hover:bg-sky-300/10 disabled:bg-transparent disabled:text-sky-300 disabled:dark:text-sky-600 dark:disabled:bg-transparent disabled:cursor-not-allowed',
+      },
+      secondary: {
+        filled:
+          'bg-slate-700 text-white dark:bg-slate-800 hover:bg-slate-600 dark:hover:bg-slate-700 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-slate-300 disabled:bg-slate-600 dark:disabled:bg-slate-600 disabled:cursor-not-allowed',
+        outlined:
+          'bg-transparent text-slate-700 border border-slate-700 dark:border-slate-600 dark:text-slate-400 hover:bg-slate-100 dark:hover:bg-slate-300/10 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-slate-300 disabled:bg-slate-100 dark:disabled:bg-slate-300/10 disabled:cursor-not-allowed',
+        text: 'bg-transparent text-slate-700 dark:text-slate-400 hover:bg-slate-50 dark:hover:bg-slate-300/10 disabled:bg-transparent disabled:text-slate-400 disabled:dark:text-slate-600 dark:disabled:bg-transparent disabled:cursor-not-allowed',
+      },
+      danger: {
+        filled:
+          'bg-red-500 text-white dark:bg-red-400 hover:bg-red-600 dark:hover:bg-red-500 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-red-300 disabled:bg-red-300 dark:disabled:bg-red-300 disabled:cursor-not-allowed',
+        outlined:
+          'bg-transparent text-red-500 border border-red-500 dark:border-red-300 dark:text-red-300 hover:bg-red-50 dark:hover:bg-red-300/10 focus:outline-none focus:ring-4 dark:focus:ring-slate-600 focus:ring-red-300 disabled:bg-red-50 dark:disabled:bg-red-300/10 disabled:cursor-not-allowed',
+        text: 'bg-transparent text-red-500 dark:text-red-300 hover:bg-red-50 dark:hover:bg-red-300/10 disabled:bg-transparent disabled:text-red-300 disabled:dark:text-red-900 dark:disabled:bg-transparent disabled:cursor-not-allowed',
+      },
+    }
 
-  return (
-    <button
-      type={type}
-      className={buttonClassNames}
-      onClick={onClick}
-      disabled={isDisabled}
-    >
-      {children}
-    </button>
-  )
-}
+    const buttonClassNames = classnames(
+      'flex items-center justify-center gap-1.5 rounded-lg font-medium',
+      appearanceClassNames[appearance][variant],
+      {
+        'w-full': isFullWidth,
+        'h-6 px-4 text-xs': size === 'sm',
+        'h-8 px-5 text-sm': size === 'md',
+        'h-10 px-6 text-sm': size === 'lg',
+      },
+      {
+        'h-10 w-10 px-0': isOnlyIcon,
+      },
+    )
+
+    return (
+      <button
+        ref={ref}
+        type={type}
+        className={buttonClassNames}
+        onClick={onClick}
+        disabled={isDisabled}
+      >
+        {children}
+      </button>
+    )
+  },
+)
+Button.displayName = 'Button'
 
 export default Button
--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,135 +1,123 @@
 'use client'
-import React, { forwardRef, Fragment, ReactNode } from 'react'
-import * as Toast from '@radix-ui/react-toast'
-import {
-  CheckIcon,
-  InfoCircledIcon,
-  ExclamationTriangleIcon,
-} from '@radix-ui/react-icons'
+import React, { forwardRef, Fragment } from 'react'
+import * as RadixToast from '@radix-ui/react-toast'
 import { Transition } from '@headlessui/react'
+import classnames from 'classnames'
+import { XMarkIcon, CheckIcon } from '@heroicons/react/24/solid'
 
-const ToastProvider = Toast.Provider
-// const ToastRoot = Toast.Root
+type ClassNames = {
+  [key: string]: {
+    [key: string]: string
+  }
+}
 
-interface Prop {
-  children: ReactNode
+type Props = {
+  title?: string
+  description?: string
+  direction?: 'top' | 'center' | 'bottom' | 'right'
+  type?: 'success' | 'error'
   open: boolean
   onOpenChange: Function
-  type?: 'success' | 'error' | 'warning'
-  position?: 'top' | 'center' | 'bottom' | 'right'
 }
-const ToastRoot = forwardRef<React.ElementRef<typeof Toast.Root>, Prop>(
+
+const typeClassNames: ClassNames = {
+  success: {
+    icon: 'dark:bg-green-300/10 dark:text-green-300 bg-green-200 text-green-800',
+  },
+  error: {
+    icon: 'dark:bg-red-300/10 dark:text-red-300 bg-red-200 text-red-800',
+  },
+}
+
+const directionClassNames: ClassNames = {
+  top: {
+    root: 'left-1/2 top-6 -translate-y-1/2 -translate-x-1/2',
+    from: '-translate-y-full',
+    to: '-translate-y-0',
+  },
+  bottom: {
+    root: 'left-1/2 bottom-6 -translate-y-1/2 -translate-x-1/2',
+    from: 'translate-y-full',
+    to: 'translate-y-0',
+  },
+  center: {
+    root: 'left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2',
+    from: '-translate-y-1/2',
+    to: '-translate-y-1/2',
+  },
+  right: {
+    root: 'right-6 top-6',
+    from: 'translate-x-full',
+    to: 'translate-x-0',
+  },
+}
+
+const Toast = forwardRef<React.ElementRef<typeof RadixToast.Root>, Props>(
   (
     {
-      children,
+      title = 'Title',
+      description = 'Description',
+      direction = 'top',
+      type = 'success',
       open = false,
       onOpenChange = () => {},
-      type = 'success',
-      position = 'bottom',
     },
     ref,
   ) => {
-    let classStyle = 'bg-green-100 text-green-800'
-    let icon = <CheckIcon className="h-6 w-6" />
-    switch (type) {
-      case 'error':
-        classStyle = 'bg-red-100 text-red-800'
-        icon = <ExclamationTriangleIcon className="h-5 w-5" />
+    const rootClassNames = classnames(
+      'fixed z-50 rounded-lg ring-1 ring-slate-100 shadow-lg dark:ring-slate-800 w-96 bg-white shadow-lg p-4 text-slate-800 dark:bg-slate-900 dark:text-slate-300',
+      directionClassNames[direction]['root'],
+    )
 
-        break
-      case 'warning':
-        classStyle = 'bg-yellow-50 text-yellow-800'
-        icon = <InfoCircledIcon className="h-5 w-5" />
-        break
-    }
-
-    let style1: string = 'translate-y-full'
-    let style2: string = 'translate-y-0'
-    let positionStyle = 'left-1/2 bottom-1 -translate-y-1/2 -translate-x-1/2'
-    switch (position) {
-      case 'top':
-        style1 = '-translate-y-full'
-        style2 = '-translate-y-0'
-        positionStyle = 'left-1/2 top-1 -translate-y-1/2 -translate-x-1/2'
-        break
-      case 'center':
-        style1 = '-translate-y-1/2'
-        style2 = '-translate-y-1/2'
-        positionStyle = 'left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2'
-        break
-      case 'right':
-        style1 = 'translate-x-full'
-        style2 = 'translate-x-0'
-        positionStyle = 'right-1 top-1 '
-        break
-    }
+    const iconClassNames = classnames(
+      'mx-auto flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full',
+      typeClassNames[type]['icon'],
+    )
 
     return (
-      <Toast.Root
-        ref={ref}
-        open={open}
-        onOpenChange={() => onOpenChange()}
-        duration={100000}
-        className={`fixed z-50 rounded-lg border border-slate-100 bg-white/90 px-4 py-3 text-slate-800 dark:bg-slate-900 dark:text-slate-300 ${positionStyle}`}
-        forceMount
-        asChild
-      >
-        <Transition
-          as={Fragment}
-          show={open}
-          enter="transition ease-out duration-200"
-          enterFrom={`opacity-0 ${style1}`}
-          enterTo={`opacity-100 ${style2}`}
-          leave="transition ease-in duration-150"
-          leaveFrom={`opacity-100 ${style2}`}
-          leaveTo={`opacity-0 ${style1}`}
+      <RadixToast.Provider>
+        <RadixToast.Root
+          ref={ref}
+          open={open}
+          onOpenChange={() => onOpenChange()}
+          duration={3000}
+          className={rootClassNames}
+          forceMount
+          asChild
         >
-          <div className="flex gap-2">
-            <div
-              className={`mx-auto flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full ${classStyle}`}
-            >
-              {icon}
+          <Transition
+            as={Fragment}
+            show={open}
+            enter="transition ease-out duration-200"
+            enterFrom={`opacity-0 ${directionClassNames[direction]['from']}`}
+            enterTo={`opacity-100 ${directionClassNames[direction]['to']}`}
+            leave="transition ease-in duration-150"
+            leaveFrom={`opacity-100 ${directionClassNames[direction]['to']}`}
+            leaveTo={`opacity-0 ${directionClassNames[direction]['from']}`}
+          >
+            <div className="flex items-center gap-3">
+              <div className={iconClassNames}>
+                {type === 'success' ? (
+                  <CheckIcon className="h-4 w-4" />
+                ) : (
+                  <XMarkIcon className="h-4 w-4" />
+                )}
+              </div>
+              <div className="flex w-full flex-1 flex-col gap-1">
+                <RadixToast.Title className="text-sm font-bold text-slate-600 dark:text-slate-50">
+                  {title}
+                </RadixToast.Title>
+                <RadixToast.Description className="text-xs text-slate-600 dark:text-slate-400">
+                  {description}
+                </RadixToast.Description>
+              </div>
             </div>
-            <div className="w-full flex-1">{children}</div>
-          </div>
-        </Transition>
-      </Toast.Root>
+          </Transition>
+        </RadixToast.Root>
+        <RadixToast.Viewport />
+      </RadixToast.Provider>
     )
   },
 )
-ToastRoot.displayName = 'ToastRoot'
-
-const ToastTitle = forwardRef<
-  React.ElementRef<typeof Toast.Title>,
-  { children: ReactNode }
->(({ children }, ref) => <Toast.Title ref={ref}>{children}</Toast.Title>)
-ToastTitle.displayName = 'ToastTitle'
-
-const ToastDescription = forwardRef<
-  React.ElementRef<typeof Toast.Description>,
-  { children: ReactNode }
->(({ children }, ref) => (
-  <Toast.Description ref={ref}>{children}</Toast.Description>
-))
-ToastDescription.displayName = 'ToastDescription'
-
-const ToastAction = forwardRef<
-  React.ElementRef<typeof Toast.Action>,
-  { children: ReactNode }
->(({ children }, ref) => (
-  <Toast.Action asChild ref={ref} altText="Goto schedule to undo">
-    {children}
-  </Toast.Action>
-))
-ToastAction.displayName = 'ToastAction'
-
-const ToastViewport = Toast.Viewport
-
-export {
-  ToastProvider,
-  ToastRoot,
-  ToastTitle,
-  ToastDescription,
-  ToastAction,
-  ToastViewport,
-}
+Toast.displayName = 'Toast'
+export default Toast
