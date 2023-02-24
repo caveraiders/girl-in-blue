9c4058a3a20331507103d8815248691c383f0034
Frank Tse <franktseme@gmail.com>
2023-02-24 22:31:52 +0800

    fix: adjust styling for Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="space-y-1 py-4 text-center text-xs leading-5 text-slate-500">
+    <footer className="flex w-full items-center justify-between space-y-1 border-t border-slate-200 bg-white p-6 text-center text-xs leading-5 dark:border-slate-800 dark:bg-slate-900">
       <div className="container mx-auto flex flex-col gap-1 px-4">
         <p>©&nbsp;Cygames,&nbsp;Inc.</p>
         <p>
