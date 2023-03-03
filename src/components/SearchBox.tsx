e39256a5c92dfceb3f50a969ea18d8a7a7487f45
kimberly <kimberlylam33@gmail.com>
2023-03-03 16:54:28 +0800

    fix: resolve search dependency and type issues

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,42 +1,38 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import { useEffect, useMemo } from 'react'
 
-interface SearchableItem {
-  [key: string]: string | number
+interface Props {
+  data: any[]
+  searchFields: string[]
+  onSearch: (results: any) => void
+  placeholder: string
+  searchTerm: string
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>
 }
 
-interface Props<T extends SearchableItem> {
-  data: T[]
-  searchFields: (keyof T)[]
-  onSearch: (results: T[]) => void
-  placeholder?: string
-  searchTerm?: string
-  setSearchTerm?: React.Dispatch<React.SetStateAction<string>>
-}
-
-function SearchBox<T extends SearchableItem>({
+function SearchBox({
   data,
   searchFields,
   onSearch,
   placeholder,
   searchTerm,
   setSearchTerm,
-}: Props<T>) {
+}: Props) {
   const searchResults = useMemo(
     () =>
       data.filter((item) => {
-        return searchFields.some((field) => {
+        return searchFields.some((field: string) => {
           return String(item[field])
             .toLowerCase()
             .includes(searchTerm.toLowerCase())
         })
       }),
-    [data, searchFields, searchTerm],
+    [searchTerm],
   )
 
   useEffect(() => {
     onSearch(searchResults)
-  }, [onSearch, searchResults])
+  }, [searchTerm])
 
   return (
     <div className="relative">
@@ -47,7 +43,7 @@ function SearchBox<T extends SearchableItem>({
         type="text"
         value={searchTerm}
         onChange={(event) => setSearchTerm(event.target.value)}
-        className="h-12 w-full rounded-lg border border-slate-700 pl-10 text-sm focus:border-sky-400 focus:ring-4 focus:ring-sky-200 dark:border-slate-600 dark:bg-slate-800 dark:placeholder-slate-600 dark:ring-slate-700 dark:focus:border-sky-400 dark:focus:ring-sky-300/20 lg:w-80"
+        className="h-12 w-full rounded-lg border border-slate-700 pl-10 text-sm focus:border-sky-400 focus:ring-4 focus:ring-sky-200 dark:border-slate-600 dark:bg-slate-800 dark:placeholder-slate-600 dark:ring-slate-700 dark:focus:border-sky-400 dark:focus:ring-sky-300/20"
         placeholder={placeholder || `Search by ${searchFields.join(' or ')}`}
       />
     </div>
