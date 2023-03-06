ec5c4061dcdd177059901ba52d47f38b519be4f3
Frank Tse <franktseme@gmail.com>
2023-03-06 10:25:41 +0800

    feat: add dark mode support to ScrollArea component

--- src/components/ScrollArea.tsx
+++ src/components/ScrollArea.tsx
@@ -7,13 +7,13 @@ type ScrollAreaProps = {
 const ScrollArea = ({ children }: ScrollAreaProps) => (
   <RadixUiScrollArea.Root className="h-full w-full overflow-hidden bg-transparent">
     <RadixUiScrollArea.Viewport className="h-full w-full">
-      <div className="py-[15px] px-5">{children}</div>
+      {children}
     </RadixUiScrollArea.Viewport>
     <RadixUiScrollArea.Scrollbar
-      className="flex touch-none select-none bg-slate-100 p-0.5 transition-colors duration-[160ms] ease-out hover:bg-slate-200 data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
+      className="flex touch-none select-none bg-slate-100 p-0.5 transition-colors duration-[160ms] ease-out hover:bg-slate-200 data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col dark:bg-transparent/20"
       orientation="vertical"
     >
-      <RadixUiScrollArea.Thumb className="relative flex-1 rounded-lg bg-slate-400 before:absolute before:top-1/2 before:left-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
+      <RadixUiScrollArea.Thumb className="relative flex-1 rounded-lg bg-slate-400 before:absolute before:top-1/2 before:left-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-[''] dark:bg-slate-700" />
     </RadixUiScrollArea.Scrollbar>
     <RadixUiScrollArea.Scrollbar
       className="flex touch-none select-none bg-slate-100 p-0.5 transition-colors duration-[160ms] ease-out hover:bg-slate-200 data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
